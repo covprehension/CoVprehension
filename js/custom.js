27c09af4f759b8b8948406329d261fd6132a971e
RoiArthurB <contact@arthurbrugiere.fr>
2020-03-20 15:28:30 +0700

    [PREVENT] Remove whitespaces in paths Prevent stupid error

--- js/custom.js
+++ js/custom.js
@@ -8,7 +8,7 @@ function loadView(id, aTag){
 	}else{ // Set view
 		switch (id){
 			case 1:
-				newContent = '<iframe src="./COVID-19 - v1.html"></iframe>';
+				newContent = '<iframe src="./COVID-19-v1.html"></iframe>';
 				aTag.nextElementSibling.style.paddingBottom = "56.25%";
 				break;			
 			//case 3:
