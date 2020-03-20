3f921147b6c5fb9dc2ecb82702197bf877b9b251
benoitgaudou <benoit.gaudou@gmail.com>
2020-03-20 15:11:32 +0700

    New version of the model v1

--- js/custom.js
+++ js/custom.js
@@ -7,6 +7,10 @@ function loadView(id, aTag){
 		aTag.nextElementSibling.removeAttribute('style');
 	}else{ // Set view
 		switch (id){
+			case 1:
+				newContent = '<iframe src="./COVID-19 - v1.html"></iframe>';
+				aTag.nextElementSibling.style.paddingBottom = "56.25%";
+				break;			
 			//case 3:
 			default:
 				newContent = '<iframe src="./COVID-19.html"></iframe>';
