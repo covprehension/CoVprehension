89b29297fb3940a90234c26b5c88e466b5eef553
benoitgaudou <benoit.gaudou@gmail.com>
2020-03-26 06:48:38 +0700

    addition of multi simulations in 1 page, test with Q1

---
+++ simulations/js/1/app-Q1.js
@@ -0,0 +1,27 @@
+particlesJS('particles-js-Q1A',
+  {
+    "simulation": {
+      "tick": 0,
+      "walking_angle": 50,
+      "transmission_distance": 40,
+      "probability_transmission": 0.3,
+      "speed": 3,
+      "number_particles": 100,
+      "scenario": "question1a"
+    }
+  }
+);
+
+particlesJS('particles-js-Q1B',
+  {
+    "simulation": {
+      "tick": 0,
+      "walking_angle": 50,
+      "transmission_distance": 40,
+      "probability_transmission": 0.3,
+      "speed": 3,
+      "number_particles": 1000,
+      "scenario": "question1a"
+    }
+  }
+);
