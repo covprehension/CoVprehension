6a9a0c9cf60237d9a1ef3337f4e9240e2ab7a8ac
benoitgaudou <benoit.gaudou@gmail.com>
2020-03-27 02:49:02 +0700

    Insertion des modèles dans la questino 2

---
+++ simulations/js/2/app-Q2.js
@@ -0,0 +1,29 @@
+particlesJS('particles-js-Q2A',
+  {
+    "simulation": {
+      "transmission_distance": 10,
+      "distanciation_distance": 30,
+      "speed":2,
+      "scenario": "Simulation 2a"
+     }
+  }
+);
+
+particlesJS('particles-js-Q2B',
+  {
+    "simulation": {
+      "transmission_distance": 10,
+      "distanciation_distance": 30,
+      "speed":2,      
+      "scenario": "Simulation 2b"
+    }
+  }
+);
+
+particlesJS('particles-js-Q2C',
+  {
+    "simulation": {
+      "scenario": "Simulation 2c"
+    }
+  }
+);
