dc45d6f3d0bcf62d805eb26c543802eb245921b3
benoitgaudou <benoit.gaudou@gmail.com>
2020-03-26 16:23:59 +0700

    Update app-Q1.js

--- simulations/js/1/app-Q1.js
+++ simulations/js/1/app-Q1.js
@@ -15,7 +15,12 @@ particlesJS('particles-js-Q1A',
 particlesJS('particles-js-Q1B',
 {
   "simulation": {
-//       "scenario": Window.LABEL_SIMULATION_2A
+    "tick": 0,
+    "walking_angle": 50,
+    "transmission_distance": 40,
+    "probability_transmission": 0.3,
+    "speed": 3,
+    "number_particles": 1000,
     "scenario": "Simulation 1b"
   }
 });
