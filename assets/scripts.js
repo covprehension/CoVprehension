e75760fc3e0c0ba6cd94a53e6c03507034176c64
RoiArthurB <contact@arthurbrugiere.fr>
2020-03-30 11:42:51 +0700

    [JS] Larger InView field + Reindent and soft clean file

--- assets/scripts.js
+++ assets/scripts.js
@@ -1,34 +1,34 @@
 $(function () {
-  $('[data-toggle="tooltip"]').tooltip()
+	$('[data-toggle="tooltip"]').tooltip()
 })
 
 /*
 	+=======
 	|  JS Simulation  |
                =======+
-*/
+               */
 
 /*
 	+=======
 	| Pause&Play
-*/
+	*/
 
 // Check if the double chart is in view
 function elementInView(elem){
-  return (($(window).scrollTop() + $(window).height()) > $(elem).offset().top + ( $(elem).parent().height()/2))
-          && ( $(window).scrollTop() < $(elem).offset().top + $(elem).parent().height()/3 ) ;
+	return (($(window).scrollTop() + $(window).height()) > ($(elem).offset().top +  $(elem).height()/4))
+			&& ( $(window).scrollTop() < ($(elem).offset().top + (3 * $(elem).height()/4)) );
 };
 
 // Pause&Play JS simulation if in view or not
 $(window).scroll(function(){
 
-  for (var i = pJSDom.length - 1; i >= 0; i--) {
-    if (elementInView($(pJSDom[i].pJS.canvas.el))) {
-      pJSDom[i].pJS.fn.custom.play();
-    } else {
-      pJSDom[i].pJS.fn.custom.pause();
-    }
-  }
+	for (var i = pJSDom.length - 1; i >= 0; i--) {
+		if (elementInView($(pJSDom[i].pJS.canvas.el).parent())) {
+			pJSDom[i].pJS.fn.custom.play();
+		} else {
+			pJSDom[i].pJS.fn.custom.pause();
+		}
+	}
 
 });
 
@@ -36,26 +36,19 @@ $(window).scroll(function(){
 /*
 	+=======
 	| Restart
-*/
-//function restartButton(tag_id, params){
-//	if ( $( "#"+tag_id ).find("a.btn").length == 0 ){
-//		$( "#"+tag_id ).append( '<a class="btn btn-info" style="color: white;" onclick=\'restartSimulation("' + tag_id + '", ' + JSON.stringify(params) + ')\'><i class="fas fa-redo-alt"></i> Recommencer la simulation</a>' );
-//	}else{
-//		console.error( "Already a tag in #" + tag_id );
-//	}
-//}
-
-function restartSimulation(tag_id, with_chart, params){
+	*/
+	function restartSimulation(tag_id, with_chart, params){
 
 	/*
 		Remove simulation from pJSDom
-	 */
-	for (var i = pJSDom.length - 1; i >= 0; i--) {
-		if ( $(pJSDom[i].pJS.canvas.el).parent().attr('id') == tag_id ){
-			pJSDom[i].pJS.fn.vendors.destroypJS();
-			break;
+		*/
+		for (var i = pJSDom.length - 1; i >= 0; i--) {
+			if ( $(pJSDom[i].pJS.canvas.el).parent().attr('id') == tag_id ){
+				pJSDom[i].pJS.fn.vendors.destroypJS();
+				break;
+			}
 		}
-	}
+
 	// Remove HTML elements from old simulations
 	$("#" + tag_id).html("");
 
